85c52599ee1e63f5c4be30f1029965751f9fb369
Rodolfo Saldivar <rodolfo saldivar@TDG-MBL-CHA067.local>
2018-11-21 21:17:31 -0600

    Initial commit

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoConnection: 'mongodb://rsaldivar:password9@ds143143.mlab.com:43143/g4-mongoose'
+}
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoConnection);
+
+const app = express();
+app.use(bodyParser.json());
+
+const ruta = require('./routes/personasRoutes');
+ruta(app);
+// require('./routes/personasRoutes')(app);
+
+app.listen(process.env.PORT || 5000);
